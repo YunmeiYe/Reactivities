23a1e57db0c9ca39b846b1c83826948b7881b277
YunmeiYe <yunmeiye.au@gmail.com>
2023-12-04 17:47:14 +1030

    feat: add ActivityForm

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -2,6 +2,7 @@ import { Grid } from "semantic-ui-react"
 import { Activity } from "../../../app/models/activity"
 import ActivityList from "./ActivityList";
 import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
 
 interface Props {
   activities: Activity[];
@@ -17,6 +18,7 @@ const ActivityDashboard = ({ activities }: Props) => {
         {activities[0] &&
           <ActivityDetails activity={activities[0]} />
         }
+        <ActivityForm />
       </Grid.Column>
     </Grid>
   )
