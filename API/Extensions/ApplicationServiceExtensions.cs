ad1305977911493d15b52664d20603b5c0f08e32
YunmeiYe <yunmeiye.au@gmail.com>
2023-12-04 15:15:42 +1030

    feat: add ApplicationServiceExtentions

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,29 @@
+using Application.Activities;
+using Application.Core;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            // Set DB Context
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return services;
+        }
+    }
+}
