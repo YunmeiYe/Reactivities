f5f83d31d1c88aeafd8229d24df829e01b8bf05d
YunmeiYe <yunmeiye.au@gmail.com>
2023-12-04 15:29:17 +1030

    feat: add CancellationToken implementation

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,28 +1,43 @@
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Persistence;
-using SQLitePCL;
 
 namespace Application.Activities
 {
     public class List
     {
-        public class Query: IRequest<List<Activity>> {}
+        public class Query : IRequest<List<Activity>> { }
         public class Handler : IRequestHandler<Query, List<Activity>>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly ILogger<List> _logger;
+            public Handler(DataContext context, ILogger<List> logger)
             {
                 _context = context;
+                _logger = logger;
             }
 
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return await _context.Activities.ToListAsync();
+                try
+                {
+                    for (var i = 0; i < 10; i++)
+                    {
+                        cancellationToken.ThrowIfCancellationRequested();
+                        await Task.Delay(1000, cancellationToken);
+                        _logger.LogInformation($"Task {i} has completed");
+                    }
+                }
+                catch (Exception ex) when (ex is TaskCanceledException)
+                {
+                    _logger.LogInformation("Task was canceled");
+                }
+                return await _context.Activities.ToListAsync(cancellationToken);
             }
         }
     }
 
-    
+
 }
