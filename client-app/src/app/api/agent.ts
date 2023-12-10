bd4bfe5b4ac994b057c0741ec347b72775a5b8f7
YunmeiYe <yunmeiye.au@gmail.com>
2023-12-10 21:06:42 +1030

    feat: added profile page and photo management feature

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -4,6 +4,7 @@ import { toast } from "react-toastify";
 import { router } from "../router/Routes";
 import { store } from "../stores/store";
 import { User, UserFormValues } from "../models/user";
+import { Photo, Profile } from "../models/profile";
 
 const sleep = (delay: number) => {
   return new Promise((resolve) => {
@@ -73,7 +74,7 @@ const Activities = {
   create: (activity: ActivityFormValues) => requests.post<void>(`/activities`, activity),
   update: (activity: ActivityFormValues) => requests.put<void>(`/activities/${activity.id}`, activity),
   delete: (id: string) => requests.del<void>(`/activities/${id}`),
-  attend: (id: string) => requests.post<void>(`/activities/${id}/attend`,{})
+  attend: (id: string) => requests.post<void>(`/activities/${id}/attend`, {})
 }
 
 const Account = {
@@ -82,9 +83,23 @@ const Account = {
   register: (user: UserFormValues) => requests.post<User>('/account/register', user)
 }
 
+const Profiles = {
+  get: (username: string) => requests.get<Profile>(`/profiles/${username}`),
+  uploadPhoto: (file: Blob) => {
+    let formData = new FormData();
+    formData.append('File', file);
+    return axios.post<Photo>('photos', formData, {
+      headers: { 'Content-type': 'multipart/form-data' }
+    })
+  },
+  setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
+  deletePhoto: (id: string) => requests.del(`/photos/${id}`)
+}
+
 const agent = {
   Activities,
-  Account
+  Account,
+  Profiles
 }
 
 export default agent;
--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -3,10 +3,10 @@ import App from "../layout/App";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import ActivityForm from "../../features/activities/form/ActivityForm";
-import TestErrors from "../../features/errors/TestError";
 import NotFound from "../../features/errors/NotFound";
 import ServerError from "../../features/errors/ServerError";
 import LoginForm from "../../features/users/LoginForm";
+import ProfilePage from "../../features/profiles/ProfilePage";
 
 export const routes: RouteObject[] = [
   {
@@ -17,7 +17,7 @@ export const routes: RouteObject[] = [
       { path: 'activities/:id', element: <ActivityDetails /> },
       { path: 'createActivity', element: <ActivityForm key='create' /> },
       { path: 'manage/:id', element: <ActivityForm key='manage' /> },
-      { path: 'errors', element: <TestErrors /> },
+      { path: 'profiles/:username', element: <ProfilePage /> },
       { path: 'not-found', element: <NotFound /> },
       { path: 'server-error', element: <ServerError /> },
       { path: 'login', element: <LoginForm /> },
