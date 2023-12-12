e1921896f98d1ef276d20d8ffe4ac2439ae12bf5
YunmeiYe <yunmeiye.au@gmail.com>
2023-12-12 23:29:55 +1030

    feat: implemented paging for server

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -3,15 +3,16 @@ using Application.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<Result<List<ActivityDto>>> { }
-        public class Handler : IRequestHandler<Query, Result<List<ActivityDto>>>
+        public class Query : IRequest<Result<PageList<ActivityDto>>> {
+            public PagingParams Params { get; set; }
+         }
+        public class Handler : IRequestHandler<Query, Result<PageList<ActivityDto>>>
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
@@ -24,16 +25,17 @@ namespace Application.Activities
                 _userAccessor = userAccessor;
             }
 
-            public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<PageList<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
-                var activities = await _context.Activities
+                var query = _context.Activities
+                .OrderBy(d => d.Date)
                 .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider, new {currentUsername = _userAccessor.GetUsername()})
-                .ToListAsync(cancellationToken);
+                .AsQueryable();
 
-                return Result<List<ActivityDto>>.Success(activities);
+                return Result<PageList<ActivityDto>>.Success(
+                    await PageList<ActivityDto>.CreateAsync(query,request.Params.PageNumber,request.Params.PageSize)
+                    );
             }
         }
     }
-
-
 }
